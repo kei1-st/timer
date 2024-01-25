3d64ef94155c913d127694c6e55895cc60ca8367
Keiichi Sato <51529803+kei1-st@users.noreply.github.com>
2024-01-25 15:51:34 +0000

    add handleClick(userName) def and call

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [userName, setUserName] = useState("unknown entity");
+
+  function handleClick(userName) {
+    setUserName(userName);
+  }
+
+  let newUserName = "unknown entity";
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {userName}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+          onChange={(e) => {
+            newUserName = e.target.value;
+          }}
+          type="text"
+        />
+        <button onClick={() => handleClick(newUserName)}>Set Name</button>
       </p>
     </section>
   );
