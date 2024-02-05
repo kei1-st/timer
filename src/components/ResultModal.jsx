dee253f8ba6d093c854783ec535d106c24f4f899
Keiichi Sato <51529803+kei1-st@users.noreply.github.com>
2024-02-05 14:33:08 +0000

    dialog の close をクリックした後に challenge ボタンがリセットされない問題を修正

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,10 +1,14 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime },
+  { targetTime, remainingTime, onReset },
   ref,
 ) {
   const dialog = useRef(null);
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const userWon = remainingTime < targetTime * 1000;
 
   useImperativeHandle(ref, () => {
     // 引数で渡された ref はこの関数に向いている
@@ -19,14 +23,15 @@ const ResultModal = forwardRef(function ResultModal(
 
   return (
     <dialog ref={dialog} className="result-modal">
-      <h2>You {result}</h2>
+      {userLost && <h2>You lost</h2>}
       <p>
         The target time was <strong>{targetTime}</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left</strong>
+        You stopped the timer with{" "}
+        <strong>{formattedRemainingTime} seconds left</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
