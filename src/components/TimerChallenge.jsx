c31daa4766907dd5ba90645fa63cb948fbe0dbe5
Keiichi Sato <51529803+kei1-st@users.noreply.github.com>
2024-02-05 13:55:35 +0000

    change how dialog popup

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -3,12 +3,14 @@ import ResultModal from "./ResultModal";
 
 export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef(null);
+  const dialog = useRef(null);
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
   function handleStart() {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dialog.current.showModal();
     }, targetTime * 1000);
 
     setTimerStarted(true);
@@ -22,7 +24,7 @@ export default function TimerChallenge({ title, targetTime }) {
 
   return (
     <>
-      {timerExpired && <ResultModal result="lost" targetTime={targetTime} />}
+      <ResultModal ref={dialog} result="lost" targetTime={targetTime} />
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
